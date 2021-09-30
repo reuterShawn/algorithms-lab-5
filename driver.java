313d9bc7a4cd276ca6290b165a711d3463dd9f1b
Shawn Reuter <reute078@morris.umn.edu>
2021-09-30 13:49:19 -0500

    Create main and add notes
    Created main and added notes for what we are supposed to do for the lab.

---
+++ driver.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+public class driver {
+
+/**
+ * 1. Develop an algorithm that given preferences for N programmers and N companies will find satisfactory pairings
+ * 2. Implement your algorithm and test it on several cases of preferences (Write a method to do this isSatisfactory )
+ * 3. Explain why algorithm is correct (ie meaning that your program stops and outputs a satisfactory pairing)
+ * 4. Find the efficiency of your algorithm in the worst case
+ */
+
+// Possibly use a while loop that once it is complete would call isSatifactory or matchSatisfactory
+
+// Write method to determine if our matches are satifactory or not
+
+
+}
